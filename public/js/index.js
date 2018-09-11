3e2a67368e09f1069b95eea2e91555aeb5988631
Joaoviana <joaopadraoviana@gmail.com>
2018-09-11 22:38:09 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -25,14 +25,15 @@ socket.on('newMessage', function(message) {
 });
 
 socket.on('newLocationMessage', function(message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target = "_blank">My current location</a>');
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 })
 
 jQuery('#message-form').on('submit', function(e) {
