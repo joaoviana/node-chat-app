228ada9528edbbd14658d39daffd9ee5ccdce9a6
Joaoviana <joaopadraoviana@gmail.com>
2018-09-11 20:34:22 +0200

    Styles added

--- public/js/index.js
+++ public/js/index.js
@@ -33,26 +33,31 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextBox =   jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val('');
   });
 });
 
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function(){
   if(!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser ;(')
-  }
+    return alert('Geolocation not supported by your browser ;(');
+}
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+      locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
